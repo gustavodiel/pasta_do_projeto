15207a379f4f01e09d084b44deb7162aba09ab5b
Gustavo Diel <gustavodiel@hotmail.com>
2021-04-01 19:38:23 -0300

    add images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,16 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
 require("bootstrap")
+
+$(document).ready(function(){
+  $('.custom-file-input').on('change',function(){
+    const fileName = $(this).val().split('\\');
+
+    $(this).next('.custom-file-label').html(fileName[fileName.length - 1]);
+  })
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- entrypoints/docker_entrypoint.sh
+++ entrypoints/docker_entrypoint.sh
@@ -6,6 +6,7 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+bundle install
 yarn install --check-files
 
 bundle exec rails s -b 0.0.0.0
