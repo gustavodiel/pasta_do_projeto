3cfd826d6a695e6b75f4f25fe0a5b01b1da74161
Gustavo Diel <gustavodiel@hotmail.com>
2021-04-06 21:34:58 -0300

    add notifications

--- app/jobs/image_process_job.rb
+++ app/jobs/image_process_job.rb
@@ -1,5 +1,9 @@
+require 'action_view'
+
 class ImageProcessJob
   include Sidekiq::Worker
+  include ActionView::Helpers::DateHelper
+  include Rails.application.routes.url_helpers
 
   def perform(image_id)
     image = Image.find(image_id)
@@ -15,5 +19,16 @@ class ImageProcessJob
 
     attributes = image.attributes.slice('title', 'primary_color', 'secondary_color', 'detail_color', 'background_color')
     ImageChannel.broadcast_to(image, attributes)
+
+    NotificationsHelper.send(
+      image.user,
+      {
+        title: 'Image processed',
+        message: "The image <strong>#{image.title}</strong> has finished in #{time_ago_in_words(image.created_at)}",
+        action_title: 'Go to image',
+        action_url: image_path(image),
+        image: 'bi-image'
+      }
+    )
   end
 end
