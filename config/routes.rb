9129e7568bcbce1b3656e61e216029f2df86ac21
Gustavo Diel <gustavo.diel@trustedhealth.com>
2022-05-24 16:49:18 -0300

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index', as: :home
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_for :users, controllers: {
-    sessions: 'user/sessions'
-  }
-
-  post 'allow_login/:token', controller: 'user/passwordless_login', action: 'allow_login', as: :allow_login
-  get 'login/:token', controller: 'user/passwordless_login', action: 'login', as: :do_login
-
-  resources :images
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
